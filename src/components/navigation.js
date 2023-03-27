2ce490d5c13324d096a002e6fa6fb415db7c8c0a
bassima_gh <ghba20wu@student.ju.se>
2023-03-27 17:09:40 +0200

    added tailwind

---
+++ src/components/navigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css';
+
+
+function Navigation() {
+  return (
+    <div className='text-2xl text-green-500'>navigation</div>
+  )
+}
+
+export default Navigation
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Navigation />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
