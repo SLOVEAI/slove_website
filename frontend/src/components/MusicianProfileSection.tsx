4d913683b3bdedf4b1c674c6278b94b9ab75206f
bassima_gh <ghba20wu@student.ju.se>
2023-04-28 18:46:24 +0200

    added most of the fields in the musician profile form

--- frontend/src/components/MusicianProfileSection.tsx
+++ frontend/src/components/MusicianProfileSection.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { HiOutlineArrowLeft } from "react-icons/hi";
+import { HiOutlineArrowLeft, HiOutlinePlus } from "react-icons/hi";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
 
 function MusicianProfileSection() {
   useEffect(() => {
@@ -20,11 +22,27 @@ function MusicianProfileSection() {
     navigate(path);
   };
 
+  // const [imageFile, setImageFile] = useState<File | null>(null);
+  // const [imagePreview, setImagePreview] = useState<string | null>(null);
+
+  // const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   e.preventDefault();
+  //   const file = e.target.files?.[0];
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       setImageFile(file);
+  //       setImagePreview(reader.result as string);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
+
   return (
     <>
       <div
         className="flex flex-col gap-[3rem] text-white font-manrope text-center text-2xl m-auto w-full md:w-4/5 xl:w-1/2 py-[8rem] mobile:py-[6rem]"
-        data-aos="fade-right"
+        data-aos="fade-left"
       >
         <HiOutlineArrowLeft
           onClick={goBack}
@@ -34,17 +52,42 @@ function MusicianProfileSection() {
         <h1 className="font-clashDisplay text-[3.5rem] text-primaryColor_lightBlue">
           Profile
         </h1>
-        <form action="#" className="flex flex-col text-left gap-5">
+        <form
+          action="#"
+          className="flex flex-col text-left gap-5 bg-[#121212] p-10 rounded-3xl"
+        >
           <div className="flex flex-col items-center">
-            <input type="file" name="profileImage" id="profileImage" />
+            {/* {imagePreview ? (
+              <div className="rounded-full p-10 border border-white object-fill">
+                <img
+                  src={imagePreview}
+                  alt="Image preview"
+                  className="object-fill"
+                />
+              </div>
+            ) : (
+              <div className="rounded-full p-10 border border-white">img</div>
+            )}
+            <input type="file" onChange={handleImageChange} accept="image/*" /> */}
             <label htmlFor="profileImage">Choose profile image</label>
           </div>
+          <h2 className="text-primaryColor_lightBlue">General Information</h2>
           <label htmlFor="bandName">Band name</label>
           <input
             type="text"
             name="bandName"
             id="bandName"
             placeholder="Put your band name"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+
+          <label htmlFor="Country">Country</label>
+          <input
+            type="text"
+            name="country"
+            id="country"
+            placeholder="Put the country name"
             className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
             required
           />
@@ -60,14 +103,45 @@ function MusicianProfileSection() {
           />
 
           <label htmlFor="bio">Bio</label>
-          <input
-            type="text"
-            name="bio"
-            id="bio"
-            placeholder="Put your bio"
-            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+          <textarea
+            name="message"
+            id="text"
+            rows={4}
+            cols={40}
+            placeholder="Ask your question here"
+            className="text-white text-sm p-4 rounded-lg bg-neutralColor_grayDarkest"
             required
           />
+
+          <h2 className="text-primaryColor_lightBlue">Music career</h2>
+
+          <Popup
+            trigger={
+              <div className="flex flex-row items-center justify-between rounded-lg p-4 bg-neutralColor_grayDarkest">
+                <h1 className="text-[1.2rem]">Musician type</h1>
+                <HiOutlinePlus className="cursor-pointer text-[2rem] ml-12 md:ml-0 hover:text-secondaryColor_purple transition ease-in-out" />
+              </div>
+            }
+            position="right center"
+            modal
+            nested
+          >
+            <div>Will add the musician type later</div>
+          </Popup>
+
+          <Popup
+            trigger={
+              <div className="flex flex-row items-center justify-between rounded-lg p-4 bg-neutralColor_grayDarkest">
+                <h1 className="text-[1.2rem]">Musician genre</h1>
+                <HiOutlinePlus className="cursor-pointer text-[2rem] ml-12 md:ml-0 hover:text-secondaryColor_purple transition ease-in-out" />
+              </div>
+            }
+            position="right center"
+            modal
+            nested
+          >
+            <div>Will add the musician genre later</div>
+          </Popup>
 
           <label htmlFor="experience">How long have you been active?</label>
           <input
@@ -88,6 +162,61 @@ function MusicianProfileSection() {
             className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
             required
           />
+          <label htmlFor="price">Average booking price?</label>
+          <input
+            type="number"
+            name="price"
+            id="price"
+            placeholder="Ticket pricing"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+
+          <h2 className="text-primaryColor_lightBlue">
+            Contact & Social media
+          </h2>
+          <label htmlFor="sns">Social media links</label>
+          <input
+            type="url"
+            name="sns"
+            id="sns"
+            placeholder="Link 1"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+          <input
+            type="url"
+            name="sns"
+            id="sns"
+            placeholder="Link 2"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+
+          <label htmlFor="steamingLink">Steaming links</label>
+          <input
+            type="url"
+            name="steamingLink"
+            id="steamingLink"
+            placeholder="Link 1"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+          <input
+            type="url"
+            name="steamingLink"
+            id="steamingLink"
+            placeholder="Link 2"
+            className="text-sm text-white px-4 py-2 rounded-lg bg-neutralColor_grayDarkest"
+            required
+          />
+
+          <button
+            type="submit"
+            className="mt-10 py-4 px-10 rounded-full bg-primaryColor_turquoise text-white hover:bg-primaryColor_darkTurquoise transition ease-in-out"
+          >
+            Next
+          </button>
         </form>
       </div>
     </>
---
+++ frontend/src/components/Popup.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Popup() {
+  return (
+    <>
+      <div className="p-10">
+        <h1>Popup</h1>
+      </div>
+    </>
+  );
+}
+
+export default Popup;
