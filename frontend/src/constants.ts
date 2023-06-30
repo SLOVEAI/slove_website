56cd8cc9a92a30e0a182cde88900057c428eb7a3
bassima_gh <ghba20wu@student.ju.se>
2023-06-30 15:30:33 +0200

    changed the backend url 3

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,3 @@
 import { isDevelopmentMode } from "./utils";
 
-export const BACKEND_URL = isDevelopmentMode
-  ? "/api"
-  : "https://test-3.eba-pztnpuuz.eu-north-1.elasticbeanstalk.com";
+export const BACKEND_URL = isDevelopmentMode ? "/api" : "https://www.slove.io";
