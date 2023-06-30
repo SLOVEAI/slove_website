3e4018aeea552fd91cbfe478a6813d0dafb6095f
bassima_gh <ghba20wu@student.ju.se>
2023-06-30 15:26:05 +0200

    changed the backend url 2

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -2,4 +2,4 @@ import { isDevelopmentMode } from "./utils";
 
 export const BACKEND_URL = isDevelopmentMode
   ? "/api"
-  : "http://test-3.eba-pztnpuuz.eu-north-1.elasticbeanstalk.com/";
+  : "https://test-3.eba-pztnpuuz.eu-north-1.elasticbeanstalk.com";
