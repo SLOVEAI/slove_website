41126ec90c6e163f111b7502df12e224b448e2ee
bassima_gh <ghba20wu@student.ju.se>
2023-07-11 13:20:02 +0200

    changed the backend link

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,3 @@
 import { isDevelopmentMode } from "./utils";
 
-export const BACKEND_URL = isDevelopmentMode
-  ? "/api"
-  : "https://backend.slove.io";
+export const BACKEND_URL = isDevelopmentMode ? "/api" : "backend.slove.io";
